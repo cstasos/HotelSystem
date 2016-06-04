5379dc79aa2c3cb2f6523fdcff7a119c8f853763
dienem <dim.mammis@gmail.com>
2016-06-04 20:46:22 +0300

    factory

---
+++ src/Factory/RoomFactory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+
+import Domain.Classies.Room;
+
+/**
+ *
+ * @author dienem
+ */
+public class RoomFactory {
+    
+    public static Room newRoom(String type){
+        return null;
+    }
+    
+}
