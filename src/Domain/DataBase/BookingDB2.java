ca9c14ab283e9177164d14607abd0610e0ca4bd1
dienem <dim.mammis@gmail.com>
2016-06-04 20:52:23 +0300

    factory

---
+++ src/Domain/DataBase/BookingDB2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain.DataBase;
+
+import Domain.Classies.Booking;
+
+/**
+ *
+ * @author dienem
+ */
+public class BookingDB2 {
+    
+    public static void addnewBooking(Booking b){
+        //SQL code add Booking
+    }
+    
+}
